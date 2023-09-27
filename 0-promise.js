29543b2dc6a5d093a8e07ac3c25b1c4eaca3f8eb
muhammed umar <muhammedumaralx@gmail.com>
2023-09-27 23:29:16 +0100

    new

---
+++ 0-promise.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-unused-vars */
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+    });
+  }
+  /* eslint-disable no-unused-vars */ 
