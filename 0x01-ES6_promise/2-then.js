0d3dcc59737c05811b801bf030fbd6f6344ed3be
muhammed umar <muhammedumaralx@gmail.com>
2023-09-27 23:43:43 +0100

    solution

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,10 @@
 /* eslint-disable no-unused-vars */
-export default function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-      if (success) {
-        resolve({ status: 200, body: 'Success' });
-      } else {
-        reject(new Error('The fake API is not working currently'));
-      }
-    });
+export default function handleResponseFromAPI(promise) {
+    return promise
+      .then(() => ({ status: 200, body: 'success' }))
+      .catch(() => new Error())
+      .finally(() => {
+        console.log('Got a response from the API');
+      });
   }
-  /* eslint-disable no-unused-vars */  
+  /* eslint-disable no-unused-vars */
